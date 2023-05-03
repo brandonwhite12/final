03360f8e0f9faf8b9774273b64f4ae09e191e080
brandonwhite12 <123538109+brandonwhite12@users.noreply.github.com>
2023-05-03 13:01:36 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function hawaii() {
+  let name = prompt("What's your name?");
+  alert(`${name}, Welcome to Hawaii! We hope you find your dream destination here.`);
+}
+
+function carribean() {
+  let name = prompt("What's your name?");
+  alert(`${name}, Welcome to the Carribean! We hope you find your dream destination here.`);
+}
+
+function europe() {
+  let name = prompt("What's your name?");
+  alert(`${name}, Welcome to Europe! We hope you find your dream destination here.`);
+}
+
+function southAmerica() {
+  let name = prompt("What's your name?");
+  alert(`${name}, Welcome to South America! We hope you find your dream destination here.`);
+}
